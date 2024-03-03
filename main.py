a0e159bbb74cbfaa016ef8e266b15dd9a12e9c2e
SidGames5 <saytleader@outlook.com>
2024-03-03 16:00:54 -0500

    A little bit of ai stuff

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ import wave
 import json
 from vosk import Model, KaldiRecognizer
 import pyaudio
+import ollama
 
 vosk_model_path = "/home/sid/Documents/code/python/voice-assistant/vosk/vosk-model-en-us-0.42-gigaspeech"
+
+
+def send_to_ai(content):
+    response = ollama.chat(
+        model="mistral",
+        messages=[
+            {"role": "user", "content": content},
+        ],
+    )
+    return response["message"]["content"]
 
 
 def main():
@@ -28,8 +39,21 @@ def main():
         if len(data) == 0:
             break
         if rec.AcceptWaveform(data):
-            result = json.loads(rec.Result())
-            print(result["text"])
+            r = json.loads(rec.Result())
+            result = r["text"]
+            action = send_to_ai(
+                "Which thing is the user trying to do: turn on the tv (tv_on), turn on the lights (lights_on), ask a question (question), other (other). Answer with respective alias provided: "
+                + result
+            )
+            print(action)
+            if "other" in action:
+                print("I'm sorry but I don't have the ability to do this yet.")
+            if "tv_on" in action:
+                print("You do not have a TV connected.")
+            if "lights_on" in action:
+                print("You do not have any lights connected.")
+            if "question" in action:
+                print(send_to_ai(result))
 
 
 if __name__ == "__main__":
