e5543e96ed71ba074deacc27b7acd10cb64780dc
SidGames5 <saytleader@outlook.com>
2024-03-18 18:04:22 -0400

    Change up some stuff and attempt to fix an issue

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ import pyttsx3
 
 version = "0.1.0"
 
+# ---------- CONFIGURATION ----------
+
 vosk_model_path = "/home/sid/Documents/code/python/voice-assistant/vosk/vosk-model-en-us-0.42-gigaspeech"
 
 # you can change the wake word to whatever you want
-wake_word = "hey ai".lower()
+wake_word = "hey sudo".lower()
+
+ai_model = "mistral"
+# you can use any LLM model supported by ollama
+
+# -----------------------------------
 
 
 def send_to_ai(content):
@@ -31,13 +38,17 @@ ttsengine = pyttsx3.init(driverName="espeak")
 def say(text):
     ttsengine.say(text)
     ttsengine.runAndWait()
-    return
+    print("debug: done talking")
+
+
+# DEBUGGING NOTES
+# "done talking" does not get printed even after it is done talking
 
 
 def main():
     print("Version: " + version)
     ttsengine = pyttsx3.init()
-    ttsengine.setProperty("rate", 125)
+    ttsengine.setProperty("rate", 100)
 
     model = Model(vosk_model_path)
     rec = KaldiRecognizer(model, 16000)
@@ -51,39 +62,20 @@ def main():
         frames_per_buffer=8000,
     )
 
+    print("Listening")
+
     while True:
-        print("Listening")
         data = stream.read(2000)
         if len(data) == 0:
-            break
+            continue
         if rec.AcceptWaveform(data):
             r = json.loads(rec.Result())
             result = r["text"]
             print(result)
             if not wake_word in result:
                 continue
-            action = send_to_ai(
-                "Which thing is the user trying to do: turn on the tv (tv_on), turn on the lights (lights_on), asking a question or giving a query (question), other (other), unable to understand the prompt (invalid). Answer with respective alias provided: "
-                + result
-            )
-            print(action)
-            if "tv_on" in action:
-                say("You do not have a TV connected.")
-                continue
-            if "lights_on" in action:
-                say("You do not have any lights connected.")
-                continue
-            if "question" in action:
-                say(send_to_ai(result))
-                continue
-
-            if "invalid" in action:
-                say("I didn't understand that.")
-                continue
-
-            if "other" in action:
-                say("I'm sorry but I don't have the ability to do this yet.")
-                continue
+            say(send_to_ai(result))
+            continue
 
 
 if __name__ == "__main__":
