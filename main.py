0d92ca2b7f7b5d2e8848b94b1cb09500d1240d23
SidGames5 <saytleader@outlook.com>
2024-03-23 09:48:38 -0400

    Improve answering

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ wake_word = "hey excalibur".lower()
 ai_model = "mistral"
 
 # this mode disables voice input and text-to-speech and requires you to type
-text_only_mode = False
+text_only_mode = True
 
 # should the clock be displayed in 24-hour time (19:44) or 12-hour time (7:44 PM)
 clock_24_hours = False
@@ -46,7 +46,7 @@ ip_to_airport_url = "http://localhost:3000"
 
 # if you would like to run ollama on a GPU server, you can change the address of the ollama server here
 # if you are running ollama locally, there is nothing you have to do
-ollama_url = "http://localhost:11434"
+ollama_url = "http://10.0.138.207:11434"
 
 
 # DO NOT EDIT THE FOLLOWING
@@ -233,7 +233,7 @@ def main():
                 ai_res = send_to_ai(
                     "Respond to the prompt and please keep your response shorter than 50 words. By the way, your name is excalibur. You don't have to announce that your name is excalibur every time I ask you a question. If you need to search the internet, you can! Just write `web_search: <insert the query here>` and nothing else in your response. I repeat, DO NOT INCLUDE ANYTHING BUT THE SEARCH QUERY IN YOUR RESPONSE IF YOU WISH TO PERFORM A WEB SEARCH. DO NOT SAY THAT YOU NEED TO PERFORM A WEB SEARCH AND YOU DON'T HAVE REAL TIME ACCESS, JUST WRITE THE WEB SEARCH PROMPT. If you are able to give a quality answer without using an internet search, DO NOT PUT THE PROMPT FOR A WEB SEARCH. If you do not need to perform a web search, please do not mention it in your response. The same thing goes for if you do need to perform a web search, just don't mention it in your response. And please don't put in the web search prompt if you want the user to search something up. In addition to a web search, you also have the ability to play music. If the user requests to play a playlist, write the following WITH THE EXACT WORDING: `play_playlist: <insert playlist name here>`. If the user does not specify the playlist name, write the following WITH THE EXACT WORDING: `play_music`. I will also provide the conversation history. Please ignore the last element of the list. "
                     + str(convo_history)
-                    + " When you give your response, pretend that you are talking directly to the user. Now here is the user's prompt: "
+                    + " Please do not write something along the lines of `based on the conversation history` in your response. When you give your response, pretend that you are talking directly to the user. Absolutely DO NOT put any special instructions in your response if the user does not explicitly state to do that. Now here is the user's prompt: "
                     + result[len(wake_word) :]
                 )
 
