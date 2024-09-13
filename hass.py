4317552db78bc3785a138a400e30690186537b9c
Sid <saytleader@outlook.com>
2024-09-13 08:49:51 -0400

    Added a getEntities function

--- hass.py
+++ hass.py
@@ -6,7 +6,19 @@ access_token = config.hass_access_token
 hass_url = config.hass_url
 
 def getEntities():
-    return
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    
+    response = requests.get(f"{hass_url}/api/states", headers=headers)
+    
+    if response.status_code == 200:
+        all_entities = response.json()
+        return all_entities
+    else:
+        print(f"Error: {response.status_code}")
+        return None
 
 def getEntitiesFromDomain(domain):
     headers = {
