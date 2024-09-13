62bbb4b29a9d8c90a162dd356ddce7a850610bb2
Sid <saytleader@outlook.com>
2024-09-13 10:10:35 -0400

    Added turn on/off functions for lights

--- hass.py
+++ hass.py
@@ -40,5 +40,42 @@ def getEntitiesFromDomain(domain):
         print(f"Error: {response.status_code}")
         return None
 
-def getDomains():
-    return
+def lightTurnOn(entity):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    
+    payload = {
+        "entity_id": entity
+    }
+    
+    url = f"{hass_url}/api/services/light/turn_on"
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Error turning on light: {e}")
+        return False
+
+def lightTurnOff(entity):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    }
+    
+    payload = {
+        "entity_id": entity
+    }
+    
+    url = f"{hass_url}/api/services/light/turn_off"
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Error turning off light: {e}")
+        return False
